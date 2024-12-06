ca4926d8baa16bbe73508842e59191581d12ed9d
Yuvraj Tyagi <106019310+yuvrajt1912@users.noreply.github.com>
2024-12-06 13:02:50 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'chat_screen.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensures binding for async tasks
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Disable the debug banner
       title: 'ChatGPT Clone',
       theme: ThemeData(
         primarySwatch: Colors.red,
+        scaffoldBackgroundColor: Colors.white, // Clean background
+        textTheme: TextTheme(
+          bodyText2: TextStyle(fontSize: 16.0, color: Colors.black),
+        ),
       ),
       home: ChatScreen(),
     );
